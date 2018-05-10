f1878b36493b524a2422b9ee889867ac0d01f71d
Peter Adeoye <cwizard2011@gmail.com>
2018-05-10 11:10:42 +0100

    fixed validation file

--- server/helpers/Validation.js
+++ server/helpers/Validation.js
@@ -104,12 +104,12 @@ export default class Validation {
       return res.status(400).json({ message: 'Meal name cannot be empty, please enter a meal name' });
     } else if (mealAvatar === undefined || mealAvatar.trim().length === 0) {
       return res.status(400).json({ message: 'meal Avatar cannot be empty, please upload image' });
-    } else if (/^[0-9]$/.test(price) === false) {
+    } else if (/^[0-9]+$/.test(price) === false) {
       res.status(400).json({ message: 'Price should only contain digits, please enter a valid price' });
     } else if (userId === undefined || userId.trim().length === 0) {
       return res.status(400).json({ message: 'userId cannot be empty, please enter user id' });
     } else if (menuId === undefined || menuId.trim().length === 0) {
-      return res.status(400).json({ message: 'menuId cannot be empty, please enter user id' });
+      return res.status(400).json({ message: 'menuId cannot be empty, please enter menu id' });
     } else if (Number.isNaN(parseInt(userId, 10))) {
       return res.status(400).json({ message: 'User Id must be a number' });
     } else if (Number.isNaN(parseInt(menuId, 10))) {
